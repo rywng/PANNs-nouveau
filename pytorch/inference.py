e710c1864f37e4e0251028596daa725745f8b8f1
Ryan <ryan@alien.gov>
2024-04-16 15:37:06 +0800

    feat(inference): Add ap and map calculation for model

--- pytorch/inference.py
+++ pytorch/inference.py
@@ -1,4 +1,8 @@
 import argparse
+import matplotlib.pyplot as plt
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+import csv
 import os
 
 import librosa
@@ -7,6 +11,20 @@ import torch
 import polars as pl
 
 from pytorch import models  # noqa: F401
+
+
+def three_classes_one_hot_encode(input: int, start=1) -> np.ndarray:
+    res = []
+    for i in range(3):
+        res.append(1 if input - start == i else 0)
+    if input > 3:
+        res[2] = 1
+    return np.array(res, dtype=np.float_)
+
+
+def convert_to_3classes(input_list: np.ndarray) -> np.ndarray:
+    input_list[2] = np.max(input_list[2:])
+    return np.resize(input_list, (3,))
 
 
 def move_data_to_device(x, device):
@@ -24,7 +42,7 @@ def get_infer_session(
     checkpoint_path,
     model_type,
     classes_num,
-    labels,
+    label_names,
     sample_rate=16000,
     window_size=512,
     hop_size=128,
@@ -68,7 +86,7 @@ def infer_audio(
     sample_rate: int,
     device: torch.device,
     model,
-    labels: list,
+    label_names: list,
     verbose=False,
 ):
     # Load audio
@@ -92,33 +110,99 @@ def infer_audio(
         if verbose:
             print(
                 "{}: {:.3f}".format(
-                    np.array(labels)[sorted_indexes[k]],
+                    np.array(label_names)[sorted_indexes[k]],
                     clipwise_output[sorted_indexes[k]],
                 )
             )
-        pred_labels.append(str(np.array(labels)[sorted_indexes[k]]))
+        pred_labels.append(str(np.array(label_names)[sorted_indexes[k]]))
 
     return clipwise_output, pred_labels
 
 
 def infer_directory(
-    dirpath: str, sample_rate: int, device: torch.device, model, labels: list
+    dirpath: str, sample_rate: int, device: torch.device, model, label_names: list
 ):
     res_list = []
     for filename in os.listdir(dirpath):
         filepath = os.path.join(dirpath, filename)
-        _, pred_label = infer_audio(filepath, sample_rate, device, model, labels)
+        _, pred_label = infer_audio(filepath, sample_rate, device, model, label_names)
         pred_label = pred_label[0]  # get the most likely one
         res_list.append({"path": filepath, "pred_label": pred_label})
 
     res_df = pl.DataFrame(res_list)
     print("Statistics:")
-    for label in labels[:3]:
+    for label in label_names[:3]:
         print(f"{label}: {res_df.filter(pl.col('pred_label') == label).shape[0]}")
 
     save_path = f"prediction-{dirpath.replace('/', '-')}.csv"
     print(f"Saving to {save_path}")
     res_df.write_csv(save_path)
+
+
+def infer_csv(
+    csv_path: str,
+    sample_rate: int,
+    device: torch.device,
+    model,
+    label_names: list,
+    output_name: str,
+):
+    truth_list = []
+    score_list = []
+
+    score_label_list = []  # This one doesn't use one-hot encoding
+
+    with open(csv_path) as in_csv:
+        reader = csv.DictReader(in_csv)
+        for line in reader:
+            audio_path = line["path"]
+
+            truth_list.append(three_classes_one_hot_encode(int(line["label"])))
+
+            infer_result = infer_audio(
+                audio_path, sample_rate, device, model, label_names
+            )
+
+            score_list.append(convert_to_3classes(infer_result[0]))
+            score_label_list.append({"path": audio_path, "label": infer_result[1][0]})
+
+    pl.DataFrame(score_label_list).write_csv(f"{output_name}.csv")
+
+    # Compute precision and recall for each class
+    truth_np = np.array(truth_list)
+    score_np = np.array(score_list)
+
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+    for i in range(truth_np.shape[1]):
+        precision[i], recall[i], _ = precision_recall_curve(
+            truth_np[:, i], score_np[:, i]
+        )
+        average_precision[i] = average_precision_score(truth_np[:, i], score_np[:, i])
+
+    # Plot precision-recall curves for each class
+    plt.figure()
+    for i in range(truth_np.shape[1]):
+        plt.plot(
+            recall[i],
+            precision[i],
+            label="Class: {} (AP = {:.2f})".format(
+                label_names[i], average_precision[i]
+            ),
+        )
+
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title(
+        "Precision-Recall curve (mAP = {:.3f})".format(
+            np.mean([i for i in average_precision.values()])
+        )
+    )
+    plt.legend(loc="best")
+    plt.savefig(f"{output_name}.png")
 
 
 if __name__ == "__main__":
@@ -139,21 +223,33 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    labels = ["Cough", "Humming"]
+    label_names = ["Cough", "Humming"]
     for _ in range(10):
-        labels.append("Negative")
+        label_names.append("Negative")
 
     device, model = get_infer_session(
-        args.checkpoint_path, args.model_type, args.classes_num, labels
+        args.checkpoint_path, args.model_type, args.classes_num, label_names
     )
 
     if os.path.isdir(args.audio_path):
-        infer_directory(args.audio_path, args.sample_rate, device, model, labels)
+        infer_directory(args.audio_path, args.sample_rate, device, model, label_names)
     elif args.audio_path.endswith(".csv"):
-        pass
+        infer_csv(
+            args.audio_path,
+            args.sample_rate,
+            device,
+            model,
+            label_names,
+            f"{args.model_type}-{args.classes_num}.csv",
+        )
     else:
         print(
             infer_audio(
-                args.audio_path, args.sample_rate, device, model, labels, verbose=True
+                args.audio_path,
+                args.sample_rate,
+                device,
+                model,
+                label_names,
+                verbose=True,
             )
         )
